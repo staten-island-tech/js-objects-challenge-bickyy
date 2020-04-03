3dfe3d644418e235b0e44d49525cb0380f0f27ce
bickyy <vicky.zhou21@sitechhs.com>
2020-04-03 09:29:41 -0400

    inspect doesnt work

---
+++ .github/index.js
@@ -0,0 +1,11 @@
+const Riya = {
+    name: "Riya",
+    dob: 2003,
+    graduated: false,
+    age: function () {
+        console.log(this);
+        return 2020 - this.dob;
+    }
+};
+
+// when we have an arrow function (=>) we dont have to say return 
